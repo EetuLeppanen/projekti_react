703d2d44fd9402ff2d38c53b826b0f118965b222
Jkirvesmaki <joonas.kirvesmaki@gmail.com>
2020-12-01 15:02:12 +0200

    sliderQ

---
+++ src/SliderQuestion.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 300,
+  },
+  margin: {
+    height: theme.spacing(3),
+  },
+}));
+
+const marks = [
+  {
+    value: 1,
+    label: '1',
+  },
+  {
+    value: 2,
+    label: '2',
+  },
+  {
+    value: 3,
+    label: '3',
+  },
+  {
+    value: 4,
+    label: '4',
+  },
+  {
+    value: 5,
+    label: '5',
+  },
+
+];
+
+function valuetext(value) {
+  return ${value};
+}
+
+
+function SliderQuestion(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography id="discrete-slider-custom" gutterBottom>
+        {props.question.title}
+      </Typography>
+      <Slider
+        defaultValue={50}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider-custom"
+        step={1}
+        min={1}
+        max={5}
+        marks={marks}
+        valueLabelDisplay="auto"
+      />
+    </div>
+  );
+}
+export default SliderQuestion;
